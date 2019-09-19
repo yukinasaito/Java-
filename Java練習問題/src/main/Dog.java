a0c0629f9ba5bf4ff2acf1ec95ed34d3b8dbdb4c
yukinasaito <saito@oysterworld.jp>
2019-09-19 19:13:47 +0900

    09/19

--- Java練習問題/src/main/Dog.java
+++ Java練習問題/src/main/Dog.java
@@ -1,10 +1,19 @@
 package main;
 
 public class Dog {
+	String name;
+	int age;
 
-	public static void main(String[] args) {
-		// ここにコードを挿入
-
+	public void SetName(String name) {
+		name = "ポチ";
 	}
 
+	public void SetAge(int age) {
+		age = 10;
+	}
+
+	public void ShowProfle() {
+		System.out.println("名前は" + name + "です。");
+		System.out.println("年齢は" + age + "です。");
+	}
 }
---
+++ Java練習問題/src/main/Run.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Run {
+
+	public static void main(String[] args) {
+		Dog name = new Dog();
+		System.out.println();
+	}
+}
