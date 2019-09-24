3e354665a5ef70c9804fdd8a026ca28c65cf6ff8
yukinasaito <saito@oysterworld.jp>
2019-09-24 17:06:52 +0900

    09/24

--- Java練習問題/src/main/Dog.java
+++ Java練習問題/src/main/Dog.java
@@ -4,8 +4,8 @@ public class Dog {
 
 	String name;
 	int age;
-	String str;
-	int gender;
+	String type;
+	String gender;
 
 	public void SetName(String name) {
 		this.name = name;
@@ -15,20 +15,62 @@ public class Dog {
 		this.age = age;
 	}
 
-	public Dog(String str) {
-		this.str = str;
+	public Dog(String type) {
+		this.type = type;
+
 	}
 
-	public int getGender() {
-		return gender;
-	}
-
-	public void SetGender(String string) {
+	public void SetGender(String gender) {
 		this.gender = gender;
 	}
 
+	public void Wow() {
+		switch (this.type) {
+		case "柴犬":
+			System.out.println("ワンワン");
+			break;
+		case "秋田犬":
+			System.out.println("キャンキャン");
+			break;
+		default:
+			System.out.println();
+			break;
+		}
+	}
+
+	public void Color() {
+		switch (this.gender) {
+		case "メス":
+			System.out.println("黒");
+			break;
+		case "オス":
+			System.out.println("茶色");
+			break;
+		default:
+			System.out.println();
+			break;
+
+		}
+	}
+
+	public void Size() {
+		switch (this.type) {
+		case "柴犬":
+			System.out.println("弱い");
+			break;
+		case "秋田犬":
+			System.out.println("強い");
+			break;
+		default:
+			System.out.println();
+			break;
+
+		}
+	}
+
 	public void ShowProfle() {
-		System.out.println(name + "は、" + age + "歳で" + str + "です。");
+		System.out.println(name + "は、" + age + "歳で" + type + "です。");
 		System.out.println("性別は" + gender + "です。");
 	}
+
 }
--- Java練習問題/src/main/Run.java
+++ Java練習問題/src/main/Run.java
@@ -3,16 +3,24 @@ package main;
 public class Run {
 
 	public static void main(String[] args) {
+
 		Dog dog = new Dog("柴犬");
 		dog.SetName("ポチ");
 		dog.SetAge(10);
 		dog.SetGender("メス");
 		dog.ShowProfle();
-
+		dog.Wow();
+		dog.Color();
+		dog.Size();
+		
 		dog = new Dog("秋田犬");
 		dog.SetName("太郎");
 		dog.SetAge(5);
+		dog.SetGender("オス");
 		dog.ShowProfle();
-
+		dog.Wow();
+		dog.Color();
+		dog.Size();
+		
 	}
 }
